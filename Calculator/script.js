eacdd7479a3187ae5f440406eeaa013c4cd9f0d7
rladuswl <khkkgon18@naver.com>
2022-03-22 15:50:34 +0900

    calculator 만들기

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,49 +1,86 @@
-// html의 element들 가져오기
-const calculator = document.querySelector('.calculator'); // calculator 엘리먼트와 그 자식 엘리먼트의 정보를 모두 담고 있음
-const buttons = calculator.querySelector('.calculator__buttons'); // calculator__buttons 엘리먼트와 그 자식 엘리먼트의 정보를 모두 담고 있음
-const operator = document.querySelector('.calculator__operator'); // calculator__operator 엘리먼트와 그 자식 엘리먼트의 정보를 모두 담고 있음
-const display = document.querySelector('.calculator__display'); // calculator__display 엘리먼트와 그 자식 엘리먼트의 정보를 모두 담고 있음
+'use strict';
+// 1. digit 버튼 클릭 시 숫자 display에 나오게 하기
+// 2. 연산자 함수 
+// 3. operator 버튼 클릭 시 display는 빈 화면으로 출력되고
+//   연산자 함수를 이용하여 계산하게 됨
+//   '=' 버튼 클릭시 계산 결과 보여줌 
+// 4. clear 버튼 클릭 시 display 리셋
 
-function calculate(n1, n2, operator) {
-    let result = 0;
+let currentOp = '',
+    currentVal = 0;
 
-    if (operator === '+') { // '+'버튼을 눌렀을 때
-        result = Number(n1) + Number(n2);
-    }
-    else if (operator === '-') { // '-'버튼을 눌렀을 때
-        result = Number(n1) - Number(n2);
-    }
-    else if (operator === '*') { // '*'버튼을 눌렀을 때
-        result = Number(n1) * Number(n2);
-    }
-    if (operator === '/') { // '/'버튼을 눌렀을 때
-        result = Number(n1) / Number(n2);
-    }
-    return String(result);
+function digitBtnHandler() {
+  const digits = document.querySelectorAll('.digit');
+  digits.forEach(digit => {
+    digit.addEventListener('click', (evt) => {
+      const display = document.querySelector('.display');
+      let targetDigit = evt.target.innerText;
+      display.value += targetDigit;
+    })
+  })
 }
 
-let firstNum = '';
-let operatorForAdvanced = '';
-let previousKey = '';
-let previousNum = '';
+function calculate(operator, val1, val2) {
+  if (operator === '+') {
+    return val1 + val2;
+  } else if (operator === '-') {
+    return val1 - val2;
+  } else if (operator === '*') {
+    return val1 * val2;
+  } else if (operator === '/') {
+    return val1 / val2;
+  }
+}
 
-buttons.addEventListener('click', function(event) {
-    const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있음
-    const action = target.classList[0]; // 클릭된 HTML 엘리먼트의 클래스 정보를 가져옴
-    const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옴
-    <br></br>
-    if (target.matches('button')) { // elem.matches(css)는 요소 elem이 주어진 css 선택자와 일치하는지 여부를 판단 - true/false
-        if (action  === 'number') { // 클릭된 HTML 엘리먼트의 클래스 이름이 'number'라면
-            if (display.textContent === '0' && operatorForAdvanced === '') { // 기존 계산기 숫자가 0이고, 오퍼레이터 버튼이 눌리지 않은 상태라면
-                display.textContent = buttonContent;
-                firstNum = display.textContent; // 첫 번째 숫자를 변수에 할당
-            }
-            else if (display.textContent !== '0' && operatorForAdvanced === '') {
+function operatorBtnHandler() {
+  const operatorBtns = document.querySelectorAll('.operator');
+  operatorBtns.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', (evt) => {
+      const display = document.querySelector('.display');
+      let displayVal = Number(display.value);
+      if (evt.target.innerText === '=') {
+        display.value = calculate(currentOp, currentVal, displayVal);
+        currentOp = '';
+        return;
+      }
+      if (currentOp === '') {
+        currentVal = Number(display.value);
+      } else {
+        currentVal = calculate(currentOp, currentVal, displayVal);
+      }
+      display.value = '';
+      currentOp = evt.target.innerText;
+    })
+  })
+}
 
-            }
-            else if (display.textContent !== '0' && operatorForAdvanced !== '') {
+function clearBtnHandler() {
+  const clearBtn = document.querySelector('.clear');
+  clearBtn.addEventListener('click', () => {
+    const display = document.querySelector('.display');
+    currentOp = '';
+    currentVal = 0;
+    display.value = '';
+  })
+}
 
-            }
-        }
+function decimalBtnHandler() {
+  const decimalBtn = document.querySelector('.decimal');
+  decimalBtn.addEventListener('click', () => {
+    const display = document.querySelector('.display');
+    let displayVal = display.value;
+    if (!displayVal.includes('.')) {
+      let addDecimal = display.value + '.';
+      display.value = addDecimal;
     }
-})
+  })
+}
+
+function init() {
+  digitBtnHandler();
+  operatorBtnHandler();
+  clearBtnHandler();
+  decimalBtnHandler();
+}
+
+init();
