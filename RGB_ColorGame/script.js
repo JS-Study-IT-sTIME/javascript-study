69706a381b2baf5be3dc7f5151c966381382b0b6
kyj <khkkgon0118@gmail.com>
2022-05-12 17:06:42 +0900

    css 수정

---
+++ RGB_ColorGame/script.js
@@ -0,0 +1,19 @@
+var squares = document.querySelectorAll(".square"); // 9개 square
+var colorDisplay = document.querySelector("#color-display"); // 문제
+var messageDisplay = document.querySelector("#result-message"); // 결과 메세지
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset"); // 재시작
+var resetButton = document.querySelector("#score"); // 클릭하면 결과 메세지 부분에 점수 출력
+var modeButtons = document.querySelectorAll(".mode"); // Easy, Hard 버튼
+var easyButton = document.querySelector(".mode"); // Easy 버튼 (square 4개)
+
+// 모드 (Easy or Hard)
+function mode() {
+
+}
+
+function squareColors() {
+    for (var i=0; i<squares.length; i++) {
+
+    }
+}
